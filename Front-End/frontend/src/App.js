9cfacd04301fa9dcebf24240926efd2695a32893
faiqiqbal37 <55352092+faiqiqbal37@users.noreply.github.com>
2024-03-04 20:18:48 +0000

    implemented the login page using react

--- Front-End/frontend/src/App.js
+++ Front-End/frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from "./screens/login/login";
+import Registration from "./screens/registration/registration";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <div>
                 <Routes> {/* Change Routes to Route */}
                     <Route path="/" element={<Login />} /> {/* Use element prop instead of component prop */}
+                    <Route path="/register" element={<Registration />} /> {/* Use element prop instead of component prop */}
                 </Routes>
             </div>
         </BrowserRouter>

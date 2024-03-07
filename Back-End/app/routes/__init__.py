1c71a087055c00fdd3510ccde65eb48af354b562
Theodoros Meras <tmeras1@sheffield.ac.uk>
2024-03-07 21:46:08 +0000

    Initial setup for devices API

--- Back-End/app/routes/__init__.py
+++ Back-End/app/routes/__init__.py
@@ -2,5 +2,6 @@ from flask import Blueprint
 
 auth_bp = Blueprint('auth', __name__)
 user_bp = Blueprint('user', __name__)
+device_bp = Blueprint('device', __name__)
 
-from . import auth, user
+from . import auth, user, device

3841993224ca6fd2e6f6e25756362791813d6a4a
freddy717 <lx20151020@gmail.com>
2024-03-10 22:42:02 +0000

    create staff router

--- Back-End/app/routes/__init__.py
+++ Back-End/app/routes/__init__.py
@@ -3,5 +3,6 @@ from flask import Blueprint
 auth_bp = Blueprint('auth', __name__)
 user_bp = Blueprint('user', __name__)
 order_bp = Blueprint('order', __name__)
+staff_bp = Blueprint('staff', __name__)
 
 from . import auth, user, order

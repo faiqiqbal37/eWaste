17cad934ecabe856b5b927a188d9471e1ee2b30e
faiqiqbal37 <55352092+faiqiqbal37@users.noreply.github.com>
2024-03-22 13:04:11 +0000

    fixed navigation bugs

--- Front-End/frontend/src/components/staffNavbar.jsx
+++ Front-End/frontend/src/components/staffNavbar.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import {StaffDrawer} from "./staffDrawer";
 import {Drawer} from "./drawer";
+import {resetPersistenceStorage, useStoreLogin} from "../stores/store-login";
+
 
 const StaffNavbar = () => {
+    const {loggedUser, updateLoggedUser} = useStoreLogin();
 
     return (
         <div className="navbar bg-base-100">
@@ -13,6 +16,19 @@ const StaffNavbar = () => {
             <div className="flex-1">
                 {<Link className="btn btn-ghost text-xl" to="/">eWaste</Link>}
             </div>
+            {loggedUser.user_id
+                ? <div className="flex-none">
+                    <button onClick={() => {
+                        resetPersistenceStorage()
+                        updateLoggedUser({})
+                        navigate("/")
+                    }
+                    } className="btn btn-square btn-ghost">
+                        Logout
+                    </button>
+                </div>
+                : null}
+
         </div>
     );
 };

48a21caed2b06d025e9154d2ce203f1f443f5613
POOJA ASHOK BEDRE <pabedre1@sheffield.ac.uk>
2024-04-23 18:05:08 +0100

    Devices on landing page

--- Front-End/frontend/src/screens/landingpage/landingpage.jsx
+++ Front-End/frontend/src/screens/landingpage/landingpage.jsx
@@ -1,12 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import eWasteImage from './ewaste2.jpg';
 import deviceLifespanImage from './ewaste3.jpg';
 import environmentalPreservationImage from './ewaste5.jpg';
-
 import dataSecurityImage from './ewaste6.jpg';
 const LandingPage = () => {
     const navigate = useNavigate();
+    const [orders, setOrders] = useState([]);
+    let baseUrl = "http://127.0.0.1:5000/api";
+    // Fetch data on component mount
+    useEffect(() => {
+        async function fetchAllOrders() {
+            try {
+                // Fetch orders data based on user_id from the backend API
+                const ordersResponse = await fetch(`${baseUrl}/orders`);
+                if (!ordersResponse.ok) {
+                    throw new Error('Failed to fetch orders');
+                }
+                const orders = await ordersResponse.json();
+
+                // Fetch all devices from the backend API
+                const devicesResponse = await fetch(`${baseUrl}/devices`);
+                if (!devicesResponse.ok) {
+                    throw new Error('Failed to fetch devices');
+                }
+                const devices = await devicesResponse.json();
+
+                const usersResponse = await fetch(`${baseUrl}/users`);
+                if (!usersResponse.ok) {
+                    throw new Error('Failed to fetch devices');
+                }
+                const users = await usersResponse.json();
+
+                // Map all devices to their orders based on the device_id
+                const filteredOrders = orders
+                    .filter(order => order.status === "Processed")
+                    .map(order => {
+                        const device = devices.find(device => device.device_id === order.device_id);
+                        const user = users.find(user => user.user_id === order.user_id);
+                        return {
+                            name: user.name,
+                            device_name: device.device_name,
+                            device_type: device.device_type,
+                            classification: device.classification,
+                            order_id: order.order_id,
+                            date: order.date,
+                            status: order.status,
+                            price: device.price,
+                            photos: device.photos,
+                        };
+                    });
+
+                console.log(orders)
+                setOrders(filteredOrders)
+                return filteredOrders;
+            } catch (error) {
+                console.error('Error fetching filtered orders:', error);
+                return []; // Return an empty array or handle error as needed
+            }
+        }
+
+        fetchAllOrders()
+    }, []);
 
     return (
         <>
@@ -45,12 +100,46 @@ const LandingPage = () => {
                     </div>
                 </div>
             </div>
+            {/* Device Section */}
+            <section className="bg-gray-100 py-12">
+                <div className="max-w-7xl mx-auto">
+                    <h2 className="text-3xl font-semibold text-center mb-6">Available Devices</h2>
+                    <div className="flex items-center justify-center h-full p-4">
+                        <div className="products flex flex-wrap justify-center gap-4 overflow-y-auto">
+                            {orders.map((order) => (
+                                <div className="card card-compact w-80 bg-base-100 shadow-xl"
+                                     key={order.id}> {/* Assuming orders have an 'id' property */}
+                                    <figure>
+                                        <img
+                                            src={order['photos'].length > 0 ? order['photos'][0] : "https://placehold.co/600x400"}
+                                            alt=""></img>
+                                    </figure>
+                                    <div className="card-body">
+                                        <h2 className="card-title">{order.device_name}</h2>
+                                        <p>Device Type: {order.device_type}</p>
+                                        <p>Category: {order.classification.toString().toUpperCase()}</p>
+                                        <p>Date of Order: {order.date}</p>
+                                        <p>Price: {order.price}</p>
+                                        <p>Order By: {order.name}</p>
+                                        {/*<div className="card-actions justify-end">*/}
+                                        {/*    <button className="btn btn-primary">Buy Now</button>*/}
+                                        {/*</div>*/}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </section>
 
             {/* Mission Section */}
             <section className="bg-gray-100 py-12">
                 <div className="max-w-7xl mx-auto">
                     <h2 className="text-3xl font-semibold mb-6">Mission</h2>
-                    <p className="text-lg">Empowering a sustainable future, our mission at eWaste is to revolutionize the lifecycle of electronic devices. We aim to inspire responsible consumption by providing a seamless platform for recycling and selling unused gadgets. Through cutting-edge technology and a commitment to data security, we strive to maximize the value of eWaste, promote environmental stewardship, and contribute to a greener, smarter world.</p>
+                    <p className="text-lg">Empowering a sustainable future, our mission at eWaste is to revolutionize
+                        the lifecycle of electronic devices. We aim to inspire responsible consumption by providing a
+                        seamless platform for recycling and selling unused gadgets. Through cutting-edge technology and
+                        a commitment to data security, we strive to maximize the value of eWaste, promote environmental stewardship, and contribute to a greener, smarter world.</p>
                 </div>
             </section>
 
